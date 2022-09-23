cfd45547b771068622a6cf412f682d2ed2dca2d2
marykoti06 <113008668+marykoti06@users.noreply.github.com>
2022-09-23 11:31:43 +0530

    Update hi.java

--- hi.java
+++ hi.java
@@ -1,10 +1,6 @@
-class FactorialExample{  
- public static void main(String args[]){  
-  int i,fact=1;  
-  int number=5;//It is the number to calculate factorial    
-  for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  System.out.println("Factorial of "+number+" is: "+fact);    
- }  
-}  
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
